a8fd460ea3d5288d5da9bd2d80362bb1605ac028
VickyNarotamo <vickynarotamo18@gmail.com>
2025-09-04 21:32:32 +0100

    Initialize Next.js portfolio project
    Set up new Next.js project with:
    - TypeScript
    - ESLint
    - Tailwind CSS
    - App Router (with src/ structure)
    - Initial clean up of homepage (custom heading)

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main classname="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Hello, I'm Vicky Narotamo 👋</h1>
+    </main>
+  );
+}
