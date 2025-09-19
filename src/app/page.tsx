88df8ea9046edefbd4b8cd90baf9598f4c60f5a6
VickyNarotamo <vickynarotamo18@gmail.com>
2025-09-19 17:43:12 +0100

    Removed Return from <h1>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen">
-      return <h1>Hello, I&apos;m Vicky Narotamo 👋</h1>
+      <h1>Hello, I&apos;m Vicky Narotamo 👋</h1>
     </main>
   );
 }
